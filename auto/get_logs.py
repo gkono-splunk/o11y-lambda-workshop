b72dd3d55dd1bc6b8f800c06ca6f378d38c23fc1
gkono-splunk <72992768+gkono-splunk@users.noreply.github.com>
2024-10-26 02:26:10 -0400

    Update get_logs.py

--- auto/get_logs.py
+++ auto/get_logs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import argparse
-import boto3
-import os
+import shlex
+import subprocess
+import sys
+# from botocore.session import Session
 
 
 # Create CloudWatchLogs client
-cloudwatch_logs = boto3.client('logs')
+cloudwatch_logs = Session().create_client('logs')
 
 parser = argparse.ArgumentParser(
     prog='get_logs',
@@ -21,10 +23,38 @@ parser.add_argument(
 
 function_type = parser.parse_args()
 
-
-
 if function_type.function in ['producer', 'consumer']:
-    live_tail_command = f"aws --region us-east-1 logs start-live-tail --log-group-identifiers $(terraform output -raw {function_type.function}_log_group_arn)"
-    os.system(live_tail_command)
+    """
+    # Get Log Group ARN
+    log_group_arn = [subprocess.run(
+        shlex.split(
+            f"terraform output -raw {function_type.function}_log_group_arn"),
+        stdout=subprocess.PIPE,
+        text=True
+    ).stdout]
+
+    # Start Live Tail
+    response = cloudwatch_logs.start_live_tail(logGroupIdentifiers=log_group_arn)
+    event_stream = response['responseStream']
+    for event in event_stream:
+        if event['sessionUpdate']['sessionResults']:
+            print(event)
+    """
+
+    # Get Log Group Name
+    log_group_name = subprocess.run(
+        shlex.split(
+            f"terraform output -raw {function_type.function}_log_group_name"),
+        stdout=subprocess.PIPE,
+        text=True
+    ).stdout
+    
+    live_tail_command = f"aws logs tail --log-group-identifiers {log_group_name}"
+
+    with open(f"{function_type.function}.log", "wb") as f:
+        process = subprocess.run(shlex.split(live_tail_command), stdout=subprocess.PIPE)
+        for line in iter(process.stdout.readline, b""):
+            sys.stdout.write(line)
+            f.write(line)
 else:
     pass

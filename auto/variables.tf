6146fbf110ed39fc2e4bb054111376464b729701
gkono-splunk <72992768+gkono-splunk@users.noreply.github.com>
2024-10-25 22:05:02 -0400

    Update variables.tf

--- auto/variables.tf
+++ auto/variables.tf
@@ -1,25 +1,19 @@
 variable "o11y_access_token" {
   description = "Splunk Observability Cloud :: Access Token"
   type        = string
-  default     = ""
 }
 
 variable "o11y_realm" {
   description = "Splunk Observability Cloud :: Realm"
   type        = string
-  default     = ""
-
 }
 
 variable "otel_lambda_layer" {
   description = "Splunk OpenTelemetry Lambda Layer"
   type        = list(string)
-  default     = ""
-
 }
 
 variable "prefix" {
   description = "Unique string for customiing resource names"
   type        = string
-  default     = ""
 }

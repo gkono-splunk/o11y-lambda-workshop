e8ea8295757b5e75f49fceb4d562d2556c4d2a9a
gkono-splunk <72992768+gkono-splunk@users.noreply.github.com>
2024-10-25 23:28:56 -0400

    Update get_logs.py

--- auto/get_logs.py
+++ auto/get_logs.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import argparse
+import boto3
 import os
 
+
+# Create CloudWatchLogs client
+cloudwatch_logs = boto3.client('logs')
 
 parser = argparse.ArgumentParser(
     prog='get_logs',
@@ -16,6 +20,8 @@ parser.add_argument(
 )
 
 function_type = parser.parse_args()
+
+
 
 if function_type.function in ['producer', 'consumer']:
     live_tail_command = f"aws --region us-east-1 logs start-live-tail --log-group-identifiers $(terraform output -raw {function_type.function}_log_group_arn)"

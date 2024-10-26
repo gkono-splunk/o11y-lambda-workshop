4e399c33a96072551c234e2469f868e2a434165e
gkono-splunk <72992768+gkono-splunk@users.noreply.github.com>
2024-10-26 12:31:17 -0400

    Create functions in get_logs.py
    Created 2 functions in get_logs.py:
    -> get_log_group_arn(service)
    -> start_live_tail(log_group_arn)
    
    Also imported pprint to clean up event printing.

--- auto/get_logs.py
+++ auto/get_logs.py
@@ -4,57 +4,61 @@ import argparse
 import shlex
 import subprocess
 import sys
-# from botocore.session import Session
+from botocore.session import Session
+from pprint import pprint
 
 
 # Create CloudWatchLogs client
-# cloudwatch_logs = Session().create_client('logs')
+cloudwatch_logs = Session().create_client('logs')
 
+# Define Argument Parser
 parser = argparse.ArgumentParser(
     prog='get_logs',
-    description='Does a live tail of either your Producer or Consumer function'
+    description='Does a live tail of either your Producer or Consumer service'
 )
 
+# Add argument and choices
 parser.add_argument(
-    "--function",
+    "--service",
     help="Choose between the producer or consumer logs",
     choices=['producer', 'consumer'],
     type=str
 )
 
-function_type = parser.parse_args()
+# Get value from parser arguments
+service = parser.parse_args().service
 
-if function_type.function in ['producer', 'consumer']:
-    """
-    # Get Log Group ARN
-    log_group_arn = [subprocess.run(
+def get_log_group_arn(service):
+    lg_arn = [subprocess.run(
         shlex.split(
-            f"terraform output -raw {function_type.function}_log_group_arn"),
+            f"terraform output -raw {service}_log_group_arn"),
         stdout=subprocess.PIPE,
         text=True
     ).stdout]
+    
+    return lg_arn
+
+def start_live_tail(log_group_arn):
+    # Get response object using Log Group ARN
+    response = cloudwatch_logs.start_live_tail(logGroupIdentifiers=log_group_arn)
+
+    # Get EventStream object from response
+    event_stream = response['responseStream']
+
+    # Get and print events from `sessionResults` list
+    for event_group in event_stream:
+        if 'sessionUpdate' in event_group.keys():
+            if len(event_group['sessionUpdate']['sessionResults']) > 0:
+                for event in event_group['sessionUpdate']['sessionResults']:
+                    pprint(event)
+                    print("\n\n")    
+
+
+if service in ['producer', 'consumer']:
+    # Get Log Group ARN
+    log_group_arn = get_log_group_arn(service)
 
     # Start Live Tail
-    response = cloudwatch_logs.start_live_tail(logGroupIdentifiers=log_group_arn)
-    event_stream = response['responseStream']
-    for event in event_stream:
-        print(event)
-    """
-
-    # Get Log Group Name
-    log_group_name = subprocess.run(
-        shlex.split(
-            f"terraform output -raw {function_type.function}_log_group_name"),
-        stdout=subprocess.PIPE,
-        text=True
-    ).stdout
-    
-    live_tail_command = f"aws logs tail {log_group_name}"
-
-    with open(f"{function_type.function}.log", "wb") as f:
-        process = subprocess.run(shlex.split(live_tail_command), stdout=subprocess.PIPE)
-        for line in iter(process.stdout.readline, b""):
-            sys.stdout.write(line)
-            f.write(line)
+    start_live_tail(log_group_arn)
 else:
     pass

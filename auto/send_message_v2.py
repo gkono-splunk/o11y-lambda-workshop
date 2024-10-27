8083ab2b0a0ecd5b575ae763349dd89811cf2387
gkono-splunk <72992768+gkono-splunk@users.noreply.github.com>
2024-10-26 20:32:08 -0400

    Update send_message_v2.py
    Fixed it to create 3 .logs files:
    - producer.logs
    - consumer.logs
    - response.logs
    Renamed some variables
    Create function to run processes in parallel

--- auto/send_message_v2.py
+++ auto/send_message_v2.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 import argparse
-# import logging
 import shlex
 import subprocess
-import sys
 from botocore.session import Session
+from multiprocessing import Process
 from pprint import pprint
 from time import sleep
 
 
 # Create CloudWatchLogs client
-cloudwatch_logs = Session().create_client('logs')
+cw_logs = Session().create_client('logs')
 
 # Define Argument Parser
 parser = argparse.ArgumentParser(
@@ -34,6 +33,7 @@ name = args.name
 superpower = args.superpower
 
 
+# Get API Gateway URL from TF outputs
 def get_url():
     return subprocess.run(
         shlex.split(
@@ -41,58 +41,70 @@ def get_url():
         stdout=subprocess.PIPE,
         text=True
     ).stdout
-  
+
+# Get Log Group ARN from TF outputs
 def get_log_group_arn(service):
-    lg_arn = [subprocess.run(
+    arn = [subprocess.run(
         shlex.split(
             f"terraform output -raw {service}_log_group_arn"),
         stdout=subprocess.PIPE,
         text=True
     ).stdout]
     
-    return lg_arn
+    return arn
 
-def start_live_tail(log_group_arn):
+# Run start_live_tail command on CloudWatchLogs client
+# Save output to (producer|consumer).logs file
+def start_live_tail(service):
+    lg_arn = get_log_group_arn(service)
+    
     # Get response object using Log Group ARN
-    response = cloudwatch_logs.start_live_tail(logGroupIdentifiers=log_group_arn)
+    response = cw_logs.start_live_tail(logGroupIdentifiers=lg_arn)
 
     # Get EventStream object from response
     event_stream = response['responseStream']
 
     # Get and print events from `sessionResults` list
-    for event_group in event_stream:
-        if 'sessionUpdate' in event_group.keys():
-            if len(event_group['sessionUpdate']['sessionResults']) > 0:
-                for event in event_group['sessionUpdate']['sessionResults']:
-                    pprint(event)
-                    print("\n\n")
+    with open(f'{service}.logs', 'a') as logger:
+        for event_grp in event_stream:
+            if 'sessionUpdate' in event_grp.keys():
+                if len(event_grp['sessionUpdate']['sessionResults']) > 0:
+                    for event in event_grp['sessionUpdate']['sessionResults']:
+                        pprint(event, stream=logger)
+                        print('\n', file=logger)
 
+# Run start_live_tail in parallel in the background
+# for both the producer and consumer functions
+def runInParallel():
+    proc = []
+    for service in ['producer', 'consumer']:
+        p = Process(target=start_live_tail, args=(service,), name=service)
+        p.start()
+        proc.append(p)
 
-if service in ['producer', 'consumer']:
-    # Get Log Group ARN
-    log_group_arn = get_log_group_arn(service)
-
-    # Start Live Tail
-    start_live_tail(log_group_arn)
-else:
-    pass
 
 # Get URL for curl command, and defind curl command
 curl_target = get_url()
 
 # Define curl command
-request = f"curl -d '{{ \"name\": \"{ name }\", \"superpower\": \"{ superpower }\" }}' { endpoint }"
+request = f"curl -d '{{ \"name\": \"{ name }\", \"superpower\": \"{ superpower }\" }}' { curl_target }"
 
+# Start background processes
+runInParallel()
+
+# Curl message `count` times
 count = 1000
 while count > 0:
     count -= 1
-    print(f"{ count } calls left")
     
     response = subprocess.run(
         shlex.split(request),
         stdout=subprocess.PIPE,
         text=True
     ).stdout
-    print(response)
+
+    with open('response.logs') as response_logger:
+        print(response, file=response_logger)
+        print(f"{ count } calls left", file=response_logger)
 
     sleep(1)
